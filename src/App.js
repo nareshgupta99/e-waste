fc8625208950eb2c4f5fca3da8ca625b785ce9c4
Naresh Gupta <nareshgupta0899@gmail.com>
2023-09-23 15:29:47 +0530

    map

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter, Route,  Routes } from 'react-router-dom';
 import Login from './component/login_signup/Login';
 import Home from './component/home/Home';
 import UserDashboard from './component/user_dashboard/UserDashboard';
+import Location from './component/location/Location';
+import { GoogleMap} from '@react-google-maps/api';
+import { Marker } from '@react-google-maps/api';
 
 
 
@@ -18,7 +21,7 @@ function App() {
           <Route path='/home' element={<Home />}></Route>
           <Route path='/login' element={<Login />}></Route>
           <Route path='/user-dashboard' element={<UserDashboard />}></Route> 
-      
+          <Route path='/location' element={<Location/>}></Route> 
           
         </Routes>
       
